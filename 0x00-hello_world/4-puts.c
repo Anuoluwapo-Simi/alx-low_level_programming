a1091bd488232a901db9c50ae644c9c2549a8dfa
Anuoluwapo-Simi <akinyemisimi11@gmail.com>
2022-09-08 16:23:09 -0700

    Task C4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,27 @@
+#include <studio.h>                                                                                                                   
+
+                                                                                                                                      
+
+/**                                                                                                                                   
+
+*main - print the string in the put function                                                                                          
+
+*                                                                                                                                     
+
+*Description: using the mainn function                                                                                                
+
+*this program prints "Programming is like building a multilingual puzzle                                                              
+
+* Return: 0                                                                                                                           
+
+*/                                                                                                                                    
+
+int main (void)                                                                                                                       
+
+{                                                                                                                                     
+
+puts("\"Programming is like building a multilingual puzzle");                                                                         
+
+return (0);                                                                                                                           
+
+}
