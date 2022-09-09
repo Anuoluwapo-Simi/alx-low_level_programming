da56461321dcd62d290a204f9dbfe1515f2f570e
Anuoluwapo-Simi <akinyemisimi11@gmail.com>
2022-09-09 14:08:55 -0700

    Task C04

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <studio.h>
 
 /**
 * main - Entry point
@@ -6,16 +6,13 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-
 {
 char alphabet;
 
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 if (alphabet == 'q')
-continue
-[A[C;
-[Acontinue;
+continue;
 
 else if (alphabet == 'e')
 continue;
