547d6a2e763cabba83ed1bb00656902766752935
Anuoluwapo-Simi <akinyemisimi11@gmail.com>
2022-09-09 16:25:02 -0700

    Task C04

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 * main - print if the number is positive, negative or zero
-* 
+*
 * Description: using the main function
 * this program prints "Programming is positive, negative or zero
 * Return: 0
@@ -9,7 +9,7 @@
 int main(void)
 {
 char ch;
-fot (ch = 'a' ; ch <= 'z' ; ch++)
+for (ch = 'a' ; ch <= 'z' ; ch++)
 {
 if (ch != 'e' && ch != 'q')
 {
