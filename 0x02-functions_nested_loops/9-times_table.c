a8037ad405176b022a5c642d77bd99e36dc838f4
Anuoluwapo-Simi <akinyemisimi11@gmail.com>
2022-09-13 12:35:33 -0700

    FNL09

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,14 +6,14 @@ void times_table(void)
 {
 int num, mult, prod;
 
+for (num = 0; num <= 9; num++)
+{
+_putchar('0');
+
 for (num = 0; mult <= 9; mult++)
 {
 _putchar(',');
 _putchar(' ');
-
-for (num = 0; num <= 9; num++)
-{
-_putchar('0');
 
 prod = num * mult;
 
