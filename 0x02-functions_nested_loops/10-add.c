eecefe733a1d8a887c7f22aae292b9325828060a
Anuoluwapo-Simi <akinyemisimi11@gmail.com>
2022-09-13 11:47:07 -0700

    FNL10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,4 +1,9 @@
-[A* @num2: The second integer to be added
+#include "main.h"
+
+/**
+* add - Adds to integers
+* @num1: The first integer to be added
+* @num2: The second integer to be added
 *
 * Return: The result of the addition
 */
