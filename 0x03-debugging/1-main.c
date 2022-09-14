6f6af8b5ab490ed39cdae973373a9a9f1e18c33f
Anuoluwapo-Simi <akinyemisimi11@gmail.com>
2022-09-14 09:30:11 -0700

    DBG01

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - causes of infinite loop
+* main - causes an infinite loop
 * Return: 0
 */
 
@@ -9,8 +9,7 @@ int main(void)
 {
 int i;
 
-printf(infinite loop incomming
-[Aprintf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
 i = 0;
 /*
