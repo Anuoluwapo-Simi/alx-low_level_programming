c2f888cbe7ae430d72add907e6fda2479e6f7953
Anuoluwapo-Simi <akinyemisimi11@gmail.com>
2022-09-21 15:33:00 -0700

    PAS007

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,19 +2,24 @@
 
 /**
 *leet - leet encoder
-*@str: string to be encoded
+*@s: string to be encoded
 *
 *Return: address of the encoded string
 */
 
-char *leet(char *str)
+char *leet(char *s)
 {
-int i = 0;
+int i, j;
+char a[] = "aAeEoOtTlL";
+char b[] = "4433007711";
 
-while (str[i] != '\0')
+for (i = 0; *(s + i); i++)
 {
-str[i] = transform(str[i]);
-i++;
+for (j = 0; j <= 9; j++)
+{
+if (a[j] ==  *(s + i))
+*(s + i) = b[j];
 }
-return (str);
+}
+return (s);
 }
