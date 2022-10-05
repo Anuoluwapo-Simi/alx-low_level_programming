25a9af9d7aa5fe83d460261512e6b483bf72cc64
Anuoluwapo-Simi <akinyemisimi11@gmail.com>
2022-10-05 06:54:24 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,14 +4,17 @@
 /**
 *main - program that prints its name, followed by a new line
 *@argc: arguments that contains argument input
-*@argv: argument that stores the string in array of char* (strings)
+*@argv: argument that stores the string in array of char
 *Return: 0
 */
+
 int main(int argc, char *argv[])
+
 {
 int num_1, num_2, mul;
 if (argc != 3)
 printf("Error\n");
+
 else
 {
 num_1 = atoi(argv[1]);
@@ -19,6 +22,7 @@ num_2 = atoi(argv[2]);
 mul = num_1 * num_2;
 printf("%d\n", mul);
 }
+
 return (0);
 }
 
