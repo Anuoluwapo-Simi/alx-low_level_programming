87b03234929afa282f947d59945ed245963a3dc5
Anuoluwapo-Simi <akinyemisimi11@gmail.com>
2022-10-10 16:39:11 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
 
@@ -21,10 +20,10 @@ printf("Name: (nil)\n");
 else
 printf("Name: %s\n", d->name);
 
-printf("Age: %f\n", d->age);
+printf("Age: %.6f\n", d->age);
 
 if (d->owner == NULL)
-printf(*Owner: (nil)\n");
+printf("Owner: (nil)\n");
 else
 printf("Owner: %s\n", d->owner);
 }
