879ac3e53afe9604978726d336541a6e73ae335b
Anuoluwapo-Simi <akinyemisimi11@gmail.com>
2022-10-12 15:38:00 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,7 +9,7 @@
 *Return: first index for which cmp doesnt return 0, or -1
 */
 
-int int_index(int *array, int size, int (*cmp)(int));
+int int_index(int *array, int size, int (*cmp)(int))
 {
 int i, r;
 
