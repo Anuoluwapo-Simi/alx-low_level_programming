af299705d2111b3b8a10f298072ae71d60349992
Anuoluwapo-Simi <akinyemisimi11@gmail.com>
2022-10-12 17:09:28 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -59,37 +59,21 @@ int op_div(int a, int b)
 	return (a / b);
 }
 
-
-
 /**
-
  * op_mod - find the remainder of the division of a by b
-
  * @a: first number
-
  * @b: second number
-
  *
-
- * Return: remainder of the division of a by b
-
+ * Return: remainder of the division of a by 
  */
 
 int op_mod(int a, int b)
-
 {
-
 	if (b == 0)
-
 	{
-
 		printf("Error\n");
-
 		exit(100);
-
 	}
-
 	return (a % b);
-
 }
 
